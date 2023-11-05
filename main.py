f13642168157082be74c3d21411adf931f76e2d3
Roman Arbuzov <lpandlipmbizkit@yandex.ru>
2023-11-06 01:11:11 +0300

    feat: добавлена точка входа, поправлен тест

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from abs_implementation.implementation import RealVelocity, Vector, RealPosition
-from abstracts.vector import Position
-from ship import Ship
+from abstracts.rotate import Rotate
+from ship import RotatableShip
 
 if __name__ == "__main__":
-    position = RealPosition()
-    velocity = RealVelocity()
-    vector = Vector(position, velocity)
-    unmovable_ship = Ship(vector)
-    unmovable_ship.move()
+    ship_1 = RotatableShip(Vector(RealPosition(), RealVelocity()), Rotate())
+    ship_2 = RotatableShip(Vector(RealPosition(1, 2), RealVelocity(7, 9)), Rotate(15))
+    ship_3 = RotatableShip(Vector(RealPosition(3, 4), RealVelocity(8, 15)), Rotate(30))
+
+    fleet = [ship_1, ship_2, ship_3]
 
